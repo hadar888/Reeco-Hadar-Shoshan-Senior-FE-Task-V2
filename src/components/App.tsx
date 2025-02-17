a0ff8583a34e5835cbd5509724e4cdab3d2555f3
הדר שושן <hadarhadar888@gmail.com>
2025-02-17 23:45:36 +0200

    change text and remove unused images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,43 +15,96 @@ const MainCarouselConatiner = styled.div(() => ({
     alignItems: 'center',
 }));
 
+const SeconderyCarouselConatiner = styled.div(() => ({
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '15px',
+}));
+
+
 const App = () => {
     return (
         <MainCarouselConatiner>
-            <Carousel byItem={false} isHorizontal={true}>
-                <Chip text={"hhhh דגד דגדג גגג h"} color={"blue"} width={500} />
-                <Chip text={"hhhh דגד דגדג גגג h"} color={"blue"} width={500} />
-                <Chip text={"hhhh דגד דגדג גגג h"} color={"blue"} width={500} />
-                <Chip text={"hhhh דגד דגדג גגג h"} color={"blue"} width={500} />
-                <ItemCard
-                    name={"עגבנייה עסיסית"}
-                    description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
-                    imageUrl={"/tomato.jpg"}
-                    width={300}
-                    height={400}
-                />
-                <ItemCard
-                    name={"עגבנייה עסיסית"}
-                    description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
-                    imageUrl={"/tomato.jpg"}
-                    width={300}
-                    height={400}
-                />
-                <ItemCard
-                    name={"עגבנייה עסיסית"}
-                    description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
-                    imageUrl={"/tomato.jpg"}
-                    width={300}
-                    height={400}
-                />
-                <ItemCard
-                    name={"עגבנייה עסיסית"}
-                    description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
-                    imageUrl={"/tomato.jpg"}
-                    width={300}
-                    height={400}
-                />
-            </Carousel>
+            <SeconderyCarouselConatiner>
+                <Carousel byItem={false} isHorizontal={true}>
+                    <Chip text={"דפדוף לפי כמות פיקסלים"} color={"blue"} width={500} />
+                    <Chip text={"ניתן לדפדף גם בעזרת המקדלת"} color={"green"} width={500} />
+                    <Chip text={"לוחצים על המעטפת המתאימה ואז על חצי המקלדת"} color={"yellow"} width={500} />
+                    <Chip text={"בדיקה נעימה"} color={"purple"} width={500} />
+                </Carousel>
+                <Carousel byItem={false} isHorizontal={true}>
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                </Carousel>
+            </SeconderyCarouselConatiner>
+            <SeconderyCarouselConatiner>
+                <Carousel byItem={true} isHorizontal={true}>
+                    <Chip text={"דפדוף לפי איברים"} color={"blue"} width={500} />
+                    <Chip text={"כל דפדוף מעביר איבר אחד"} color={"#5757f2"} width={500} />
+                    <Chip text={"איבר 3"} color={"#7d7de7"} width={500} />
+                    <Chip text={"איבר 4 ואחרון"} color={"#adadf5"} width={500} />
+                </Carousel>
+                <Carousel byItem={true} isHorizontal={true}>
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                    <ItemCard
+                        name={"עגבנייה עסיסית"}
+                        description={"עגבנייה עסיסית שפשוט מתחשק לתת לה ביס"}
+                        imageUrl={"/tomato.jpg"}
+                        width={300}
+                        height={400}
+                    />
+                </Carousel>
+            </SeconderyCarouselConatiner>
         </MainCarouselConatiner>
     )
 }
